0e04a900b8c4a7c3fd7547cd5b62f2871d091cca
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-03-26 21:44:07 +0900

    define handle_progress/3

--- lib/classification_web/live/page_live.ex
+++ lib/classification_web/live/page_live.ex
@@ -6,12 +6,40 @@ defmodule ClassificationWeb.PageLive do
     {
       :ok,
       socket
-      |> allow_upload(:image, accept: ~w(.jpg .jpge .png))
+      |> assign(:upload_file, nil)
+      |> assign(:filename, nil)
+      |> allow_upload(
+        :image,
+        accept: ~w(.jpg .jpge .png),
+        progress: &handle_progress/3,
+        auto_upload: true
+      )
     }
   end
 
   @impl true
   def handle_event("upload", _params, socket) do
     {:noreply, socket}
+  end
+
+  def handle_progress(:image, entry, socket)
+      when entry.done? == false,
+      # ファイルアップロードが完了するまでは何もしない
+      do: {:noreply, socket}
+
+  def handler_progress(:image, entry, socket) do
+    # アップロードされたファイルを読み込む
+    upload_file =
+      consume_uploaded_entries(socket, :image, fn %{path: path}, _entry ->
+        File.read(path)
+      end)
+      |> hd()
+
+    {
+      :noreply,
+      socket
+      |> assign(:upload_file, upload_file)
+      |> assign(:filename, entry.client_name)
+    }
   end
 end
