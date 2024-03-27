84be5c8846b7ea99f45fe533079db707494da26c
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-03-27 19:26:59 +0900

    update view file

--- lib/classification_web/live/page_live.ex
+++ lib/classification_web/live/page_live.ex
@@ -8,9 +8,11 @@ defmodule ClassificationWeb.PageLive do
       socket
       |> assign(:upload_file, nil)
       |> assign(:filename, nil)
+      |> assign(:ans, "")
+      |> assign(:score, nil)
       |> allow_upload(
         :image,
-        accept: ~w(.jpg .jpge .png),
+        accept: ~w(.jpg .jpeg .png),
         progress: &handle_progress/3,
         auto_upload: true
       )
@@ -19,27 +21,59 @@ defmodule ClassificationWeb.PageLive do
 
   @impl true
   def handle_event("upload", _params, socket) do
+    IO.puts("Upload start")
     {:noreply, socket}
   end
 
-  def handle_progress(:image, entry, socket)
-      when entry.done? == false,
-      # ファイルアップロードが完了するまでは何もしない
-      do: {:noreply, socket}
+  def handle_event("predict", _params, socket) do
+    tensor =
+      socket.assigns.upload_file
+      |> StbImage.read_binary!()
+      |> StbImage.to_nx()
 
-  def handler_progress(:image, entry, socket) do
-    # アップロードされたファイルを読み込む
-    upload_file =
-      consume_uploaded_entries(socket, :image, fn %{path: path}, _entry ->
-        File.read(path)
-      end)
-      |> hd()
+    %{predictions: [%{label: ans, score: score}]} =
+      Nx.Serving.batched_run(Classification.Serving, tensor)
 
     {
       :noreply,
       socket
-      |> assign(:upload_file, upload_file)
-      |> assign(:filename, entry.client_name)
+      |> assign(:ans, ans)
+      |> assign(:score, score)
     }
+  end
+
+  def handle_event("clear", _params, socket) do
+    IO.puts("socket #{inspect(socket)}")
+
+    {
+      :noreply,
+      socket
+      |> assign(:upload_file, nil)
+      |> assign(:filename, nil)
+      |> assign(:ans, "")
+      |> assign(:score, nil)
+    }
+  end
+
+  def handle_progress(:image, entry, socket) do
+    if entry.done? do
+      IO.puts("Upload done")
+      # アップロードされたファイルを読み込む
+      upload_file =
+        consume_uploaded_entries(socket, :image, fn %{path: path}, _entry ->
+          File.read(path)
+        end)
+        |> hd()
+
+      {
+        :noreply,
+        socket
+        |> assign(:upload_file, upload_file)
+        |> assign(:filename, entry.client_name)
+      }
+    else
+      IO.puts("Uploading...")
+      {:noreply, socket}
+    end
   end
 end
